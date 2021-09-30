a7fb47c4f750d762cc7ae1fe5971b17584813392
luis felipe <luis.s.mendes20@gmail.com>
2021-09-30 15:51:04 -0300

    ADD: Add scripts sqls, e json postman

---
+++ Back-end/Scripts_sql/db_minhalojadegames_tb_categoria.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_minhalojadegames
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_categoria`
+--
+
+DROP TABLE IF EXISTS `tb_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_categoria` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(20) NOT NULL,
+  `distribuidora` varchar(20) NOT NULL,
+  `plataforma` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_categoria`
+--
+
+LOCK TABLES `tb_categoria` WRITE;
+/*!40000 ALTER TABLE `tb_categoria` DISABLE KEYS */;
+INSERT INTO `tb_categoria` VALUES (1,'AVENTURA','NINTENDO','NINTENDO SWITCH'),(2,'CLASSICOS','NINTENDO','NINTENDO SWITCH'),(3,'AVENTURA','SONY','PS4'),(4,'ESPORTES','EA-SPORTS','PS4-XBOX-SWITCH'),(5,'TIRO - 1 PESSOA','XBOX','XBOX-ONE'),(7,'CLASSICO','ATARI','ATARI-300'),(8,'EASPORTS','KONAMI','\'PS4-XBOX-SWITCH\'');
+/*!40000 ALTER TABLE `tb_categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-30 12:26:47
---
+++ Back-end/Scripts_sql/db_minhalojadegames_tb_produtos.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_minhalojadegames
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_produtos`
+--
+
+DROP TABLE IF EXISTS `tb_produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_produtos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `data` datetime(6) DEFAULT NULL,
+  `estoque_produto` int NOT NULL,
+  `preco` double NOT NULL,
+  `produto` varchar(50) NOT NULL,
+  `categoria_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK555mlvcyvwis1o07pywhrr83p` (`categoria_id`),
+  CONSTRAINT `FK555mlvcyvwis1o07pywhrr83p` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_produtos`
+--
+
+LOCK TABLES `tb_produtos` WRITE;
+/*!40000 ALTER TABLE `tb_produtos` DISABLE KEYS */;
+INSERT INTO `tb_produtos` VALUES (1,'2021-09-30 10:52:24.000000',20,250,'pokemon Diamond',2),(2,'2021-09-30 11:37:12.032000',10,250,'pokemon Pearl',2),(4,'2021-09-30 11:05:28.000000',0,99,'Fifa 14',4),(5,'2021-09-30 11:05:47.000000',0,55.99,'PES 213',8);
+/*!40000 ALTER TABLE `tb_produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-30 12:26:48
